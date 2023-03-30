cdfebfb76f4a428bc4c48afcb2e4916eb3ff29ee
Alexa Hernandez <111934131+AlexaHernandez06@users.noreply.github.com>
2023-03-30 21:17:21 +0000

    Thu, Mar 30, 2023, 2:17 PM -07:00

--- js/aboutme.js
+++ js/aboutme.js
@@ -2,7 +2,7 @@ function revealMessage() {
     document.getElementById("hiddenMessage").style.display = 'block';
 }
 
-function countdDown() {
+function countDown() {
     var currentVal = document.getElementById("countDownButton").innerHTML;
     var newVal = 0;
 

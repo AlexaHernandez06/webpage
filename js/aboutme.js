f97a9d662dbbd09e1bdb7e3fc5010c4be6c16f37
Alexa Hernandez <111934131+AlexaHernandez06@users.noreply.github.com>
2023-03-30 02:23:13 +0000

    Wed, Mar 29, 2023, 7:23 PM -07:00

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,20 +1,4 @@
-$("h1").each(function(index, element){
-    var animation = TweenMax.to(this, 0.2, {
-      className: '+= superShadow',
-      marginTop: '-10px',
-      marginBottom: '10px',
-      ease: Power1.easeIn,
-      paused:true
-    });
-    element.animation = animation;
-  })
 
-
-  $('h1').hover(function(){
-   this.animation.play()
-  }, function(){
-   this.animation.reverse();
-  })
 
 
 
